cddef5c8fefd32c7a4433f82c46079d9ad722f2e
Manmit Singh <49245208+SupremeSingh@users.noreply.github.com>
2022-07-14 15:55:05 -0400

    With a shares based approach

--- splitter.sol
+++ splitter.sol
@@ -2,80 +2,71 @@
 pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/interfaces/IERC20.sol";
-
-contract SplitterFactory {
-
-    mapping(address => mapping(address => bool)) userAccountDetails;
-    
-    event NewSplitterCreated(address indexed creator, address splitter);
-
-    function deploy(address tokenAddress) external {
-        require(userAccountDetails[msg.sender][tokenAddress] == false, "You have a splitter for this token");
-        Splitter newSplitterInstance = new Splitter(tokenAddress);
-        userAccountDetails[msg.sender][tokenAddress] = true;
-        emit NewSplitterCreated(msg.sender, address(newSplitterInstance));
-
-        newSplitterInstance.transferOwnership(msg.sender);
-    }
-}
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 
 contract Splitter is Ownable { 
 
-    IERC20 tokenToBeSplit;
+    using SafeERC20 for IERC20;
+    IERC20 immutable tokenToBeSplit;
 
-    struct UserAccounts {
-        uint256 balances;
-        bool hasBeenRegistered;
-    }
+    mapping (address => uint256) userAccountDetails;
 
-    address[] public allAddressesRegistered; 
-    mapping (address => UserAccounts) userAccountDetails;
+    uint256 public totalValueDeposited;
+    uint256 public currentShareValue;
+    uint256 public totalSharesIssued;
 
     event NewParticipantAdded(address indexed newParticipant);
     event DepositTokens(address contributor, uint256 amountAdded);
     event WithdrawTokens(address indexed withdrawer, uint256 amountWithdrawn);
-
-    constructor(address _tokenAddress) {
+    
+    constructor(address _tokenAddress, address[] memory initialUserList, uint256 baseShares) {
+      require(baseShares % 1000000000 > 0, "Base shares must be at least 9 decimals");
       require(IERC20(_tokenAddress).totalSupply() >= 0, "This is not a valid ERC20 Token");
+      
       tokenToBeSplit = IERC20(_tokenAddress);  
+      for (uint256 i = 0; i < initialUserList.length; ++i ) {
+          _addNewUser(initialUserList[i], baseShares);
+      }
+      totalSharesIssued += baseShares * initialUserList.length;
     }
 
-    function depositTokens(uint256 _amount) external onlyOwner() {
-        tokenToBeSplit.transferFrom(msg.sender,address(this), _amount);
-        emit DepositTokens(msg.sender, _amount);
-        updateUserBalances(_amount);
+    function depositTokens(uint256 amount) external onlyOwner() {
+        currentShareValue = amount / totalSharesIssued;
+        totalValueDeposited += amount;
+        emit DepositTokens(msg.sender, amount);
+        tokenToBeSplit.safeTransferFrom(msg.sender,address(this), amount);
     }
 
-    function withdrawTokens(uint256 _amountToBeWithdrawn) external {
-        require(msg.sender != owner(), "Owner cannot withdraw funds from the contract");
-        require(userAccountDetails[msg.sender].balances >= _amountToBeWithdrawn, "Insufficient balance in account");
-        userAccountDetails[msg.sender].balances -= _amountToBeWithdrawn;
-        emit WithdrawTokens(msg.sender, _amountToBeWithdrawn);
-        tokenToBeSplit.transfer(msg.sender, _amountToBeWithdrawn);
+    function addNewUser(address newUserAddress, uint256 baseShares) external onlyOwner() {
+        require(userAccountDetails[newUserAddress] > 0, "This address has been registered");
+        currentShareValue = totalValueDeposited / (totalSharesIssued + baseShares);
+        _addNewUser(newUserAddress, baseShares);
     }
 
-
-    function addParticipants(address _newUserAddress) external onlyOwner {
-        require(_newUserAddress != address(0) && _newUserAddress != owner(), "This address is not a valid input");
-        require(!userAccountDetails[_newUserAddress].hasBeenRegistered, "This address has been registered");
-        allAddressesRegistered.push(_newUserAddress);
-        userAccountDetails[_newUserAddress] = UserAccounts(0, true);
-        emit NewParticipantAdded(_newUserAddress);
+    function withdrawDust() external onlyOwner() {
+        uint256 dustAmount = tokenToBeSplit.balanceOf(address(this)) - totalValueDeposited;
+        require(dustAmount > 0, "Not enough dust");
+        tokenToBeSplit.safeTransfer(msg.sender, dustAmount);
+        emit WithdrawTokens(msg.sender, dustAmount);
     }
 
-    function getContractBalance() public view returns(uint256 value){
-        value = tokenToBeSplit.balanceOf(address(this));
+    function withdrawTokens(uint256 amountToBeWithdrawn) external {
+        require(userAccountDetails[msg.sender] * currentShareValue >= amountToBeWithdrawn, "Insufficient balance in account");
+        userAccountDetails[msg.sender] -= amountToBeWithdrawn / currentShareValue;
+        totalSharesIssued -= amountToBeWithdrawn / currentShareValue;
+        totalValueDeposited -= amountToBeWithdrawn;
+        emit WithdrawTokens(msg.sender, amountToBeWithdrawn);
+        tokenToBeSplit.safeTransfer(msg.sender, amountToBeWithdrawn);
     }
 
-    function updateUserBalances(uint256 _amount) internal returns(bool success) {
-        uint256 intVal = _amount / allAddressesRegistered.length;
-        uint256 carry = _amount % allAddressesRegistered.length;
-        for (uint256 i = 0; i < allAddressesRegistered.length; ++i ) {
-            userAccountDetails[allAddressesRegistered[i]].balances += intVal;
-        }
-        tokenToBeSplit.transfer(owner(), carry);
-        success = true;
+    function _addNewUser(address newUserAddress, uint256 shares) private {
+        require(shares > 0, "Share value cannot be 0");
+        require(shares % 1000000000 > 0, "Base shares must be at least 9 decimals");
+        require(newUserAddress != address(0), "0x - This address is not a valid input");
+        
+        userAccountDetails[newUserAddress] = shares;
+        emit NewParticipantAdded(newUserAddress); 
     }
 
 }
