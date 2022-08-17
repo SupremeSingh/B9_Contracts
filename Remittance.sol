46417c69220e48650bfa4d581121264cebc268fc
Manmit Singh <49245208+SupremeSingh@users.noreply.github.com>
2022-08-17 18:29:16 -0400

    a new contract added

--- Remittance.sol
+++ Remittance.sol
@@ -1,40 +1,60 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract  Remittance is Ownable { 
-    
-    uint256 withdrawalTime;
-    address[] public userList;
-    mapping(address => bytes32) userToPassword;
+contract Remittance is Ownable { 
 
-    modifier isExchanger() {
-        require(msg.sender == userList[0]);
+    uint256 public totalFundsToSend;
+    uint256 public reviewDuration;
+    bytes32 public uniqueSignatureValue;
+    bool public investigationStatus;
+
+    mapping(address => uint256) agentToEthBalance;
+
+    event ReleaseTokensToExchanger(address exchangerAddress, uint256 timestamp);
+    event SubmitFraudProof(address recipientAddress, uint256 timestamp);
+    event InvestigationConcluded(address addressPunished, uint256 fundsRetained, uint256 timestamp);
+
+    modifier whileInReview() {
+        require(block.timestamp < reviewDuration, "Can only do this in the review period");
         _;
     }
 
-    event WithdrawFunds(address indexed withdrawer, uint256 amountWithdrawn);
+    constructor(uint256 fundsToSend, uint256 withholdingPeriod, bytes32 uniqueSignature) payable {
+        require(msg.value >= fundsToSend, "Not enough funding provided");
+        require(withholdingPeriod >= 1 hours, "At least 1 hour of waiting is needed");
+        reviewDuration = block.timestamp + (withholdingPeriod * 1 hours);
+        totalFundsToSend = fundsToSend;
+        uniqueSignatureValue = uniqueSignature;
+    }   
 
-    constructor(uint256 seedAmount, address exchangerRole, address recieverRole) payable {
-        require(msg.value >= seedAmount, "Not enough seed funding provided");
-        userList.push(exchangerRole);
-        userList.push(recieverRole);
+    function releaseTokensToExchanger(uint256 exchangerNumber, uint256 recipientNumber) external whileInReview {
+        require(uniqueSignatureValue == keccak256(abi.encodePacked(exchangerNumber, recipientNumber)), "One of the inputs is invalid");
+        agentToEthBalance[msg.sender] += address(this).balance;
+        emit ReleaseTokensToExchanger(msg.sender, block.timestamp);
     }
 
-    // Implement an atomic transaction over here 
-    function withdrawFunds(uint256 exchangerPassword, uint256 receiverPassword) isExchanger() external {
-        require(msg.sender == userList[0]);
-        require(_computePasswordFromNonce(exchangerPassword) == userToPassword[userList[0]]);
-        require(_computePasswordFromNonce(receiverPassword) == userToPassword[userList[1]]);       
-        
-        emit WithdrawFunds(msg.sender, address(this).balance);
-        payable(msg.sender).transfer(address(this).balance);
+    function submitFraudProof() external payable whileInReview {
+        require(msg.value >= totalFundsToSend, "Inadequate stake for the fraud proof");
+        agentToEthBalance[msg.sender] += msg.value;
+        investigationStatus = true;
+        emit SubmitFraudProof(msg.sender, block.timestamp);
     }
 
-    function _computePasswordFromNonce(uint256 nonce) internal returns(bytes32 passwordHash) {
-        passwordHash = keccak256(abi.encodePacked(nonce));
-        userToPassword[msg.sender] = passwordHash;
+    function conductInvestigation(address agentToPunish) external onlyOwner whileInReview {
+        require(investigationStatus, "No complaint filed by recipient");
+        agentToEthBalance[msg.sender] += agentToEthBalance[agentToPunish];
+        emit InvestigationConcluded(agentToPunish, agentToEthBalance[agentToPunish], block.timestamp);
+        agentToEthBalance[agentToPunish] = 0;
+        investigationStatus = false;
     }
 
+    function withdrawValue(uint256 amount) public {
+        require(block.timestamp > reviewDuration, "Can only withdraw funds after review duration");
+        require(!investigationStatus, "Cannot withdraw during on-going investigation");
+        require(agentToEthBalance[msg.sender] >= amount, "Insufficient balance in account");
+        agentToEthBalance[msg.sender] -= amount;
+        payable(address(msg.sender)).transfer(amount);
+    } 
 }
